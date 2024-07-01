2d4b2b432d75c9bfff845e700bc10aec59817b47
junnun15 <junnunprodhan@gmail.com>
2024-07-01 22:31:27 +0600

    dashboard customize

--- src/app/(withDashboard)/dashboard/admin/page.tsx
+++ src/app/(withDashboard)/dashboard/admin/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return <div>
     <div className="mb-7.5">
   <h2 className="text-white font-bold mb-1.25">
-  Welcome to your
+  Welcome to your  
   </h2>
   <h5 className="text-green-400">
   Dashboard!!  
