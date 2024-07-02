24d801288ee023173d0870f6a912d03da65fdd89
junnun15 <junnunprodhan@gmail.com>
2024-07-02 21:28:55 +0600

    add so m

--- src/app/(withDashboard)/dashboard/admin/page.tsx
+++ src/app/(withDashboard)/dashboard/admin/page.tsx
@@ -1,14 +1,29 @@
+// import ApexChart from "@/component/Dashboard/ApexChart/ApexChart";
 import React from "react";
 
 const page = () => {
   return <div>
     <div className="mb-7.5">
   <h2 className="text-white font-bold mb-1.25">
-  Welcome to your  
+  Dashboard
   </h2>
   <h5 className="text-green-400">
-  Dashboard!!!   
+  Welcome to Admin Dashboard!!
   </h5>
+</div>
+
+<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+  <div className="bg-white rounded-lg">
+    <div className="p-2 ">
+      <h1>Total Sales</h1>
+      <div>
+        {/* <ApexChart/> */}
+      </div>
+    </div>
+  </div>
+  <div>div 2</div>
+  <div>div 3</div>
+  <div>div 4</div>
 </div>
 
   </div>;
---
+++ src/component/Dashboard/ApexChart/ApexChart.tsx
@@ -0,0 +1,39 @@
+// import React from 'react';
+// import Chart from 'react-apexcharts';
+// import { ApexOptions } from 'apexcharts';
+
+// const ApexChart: React.FC = () => {
+//   const options: ApexOptions = {
+//     chart: {
+//       id: 'basic-bar',
+//     },
+//     xaxis: {
+//       categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997],
+//     },
+//     colors: ['#10b981'], // Tailwind's green-500 color
+//     theme: {
+//       mode: 'dark',
+//     },
+//     stroke: {
+//       curve: 'smooth',
+//     },
+//     grid: {
+//       borderColor: '#374151', // Tailwind's gray-700 color
+//     },
+//   };
+
+//   const series = [
+//     {
+//       name: 'series-1',
+//       data: [30, 40, 35, 50, 49, 60, 70],
+//     },
+//   ];
+
+//   return (
+//     <div className="bg-gray-800 p-4 rounded-lg">
+//       <Chart options={options} series={series} type="line" width="100%" height="350" />
+//     </div>
+//   );
+// };
+
+// export default ApexChart;
