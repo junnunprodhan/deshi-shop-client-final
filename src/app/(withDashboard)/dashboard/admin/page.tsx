3219970b9a98b3986df941ce6302e67ca4ef0663
junnun15 <junnunprodhan@gmail.com>
2024-07-15 21:23:43 +0600

    add code

--- src/app/(withDashboard)/dashboard/admin/page.tsx
+++ src/app/(withDashboard)/dashboard/admin/page.tsx
@@ -21,7 +21,7 @@ const page = () => {
       </div>
     </div>
   </div>
-  <div>this is total earning</div>
+  <div>this is total earning </div>
   <div>this is income </div>
   <div>this is products</div>
 </div>
